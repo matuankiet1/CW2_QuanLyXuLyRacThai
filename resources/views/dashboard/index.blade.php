db802da0b4e1e1478d350235d18ff58fb8430c45
MaTuanKiet <matuankiet2003@gmail.com>
2025-11-01 10:16:08 +0700

    fix view dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.dashboard')
+@extends('layouts.admin-with-sidebar')
 
-@section('main-content')
+@section('content')
     <div class="p-6">
         {{-- Overview Cards --}}
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-6">
