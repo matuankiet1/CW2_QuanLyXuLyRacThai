d5b4fcb6b22a5eae698e2fabca63c8ef7b41db67
ThanhTamSW <buiken246@gmail.com>
2025-11-03 10:34:27 +0700

    feat(notifications): add notification badge with unread count to user navbar

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -281,6 +281,19 @@
                             <i class="fas fa-flag me-1"></i>Báo cáo
                         </a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('user.notifications.index') }}">
+                            <i class="fas fa-bell me-1"></i>Thông báo
+                            @php
+                                $unreadCount = App\Models\NotificationUser::where('user_id', auth()->user()->user_id)
+                                    ->whereNull('read_at')
+                                    ->count();
+                            @endphp
+                            @if($unreadCount > 0)
+                                <span class="badge bg-danger">{{ $unreadCount }}</span>
+                            @endif
+                        </a>
+                    </li>
                     @endauth
                 </ul>
                 
